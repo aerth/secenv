c04438df5bde04d8a1c699e0bbba2f6c99821cc6
aerth <aerth@isupon.us>
2016-03-29 00:22:07 -0700

    config.enc

--- main.go
+++ main.go
@@ -23,64 +23,68 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "os/exec"
-  "github.com/aerth/seconf"
-  "strings"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/aerth/seconf"
 )
 
 func main() {
-  if len(os.Args) > 1 {
-    if os.Args[1] == "-d" {
-      if !seconf.Detect("config.enc") { fmt.Println("No config file to delete."); os.Exit(1)}
-      fmt.Println("Removing Environment")
-      seconf.Destroy("config.enc")
-      os.Exit(1)
-    }
-  }
-  if !seconf.Detect("config.enc") && askForConfirmation("No config file found. Would you like to create one?") {
-    err := seconf.Lock("config.enc", "Secured Environment", "Enter the first environmental NAME", "Enter the first environmental VALUE", "Enter the second environmental NAME", "Enter the second environmental VALUE")
-    if err != nil{
-      fmt.Println(err)
-      os.Exit(1)
-    }
-    if len(os.Args) < 2 { os.Exit(0) }
-  }
-  if !seconf.Detect("config.enc") {
-    fmt.Println("No config.")
-    os.Exit(1)
-  }
-  if len(os.Args) < 2 {
-    fmt.Println("User error: No command supplied.")
-    fmt.Println("Usage: "+os.Args[0])
-    os.Exit(1)
-  }
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-d" {
+			if !seconf.Detect("config.enc") {
+				fmt.Println("No config file to delete.")
+				os.Exit(1)
+			}
+			fmt.Println("Removing Environment")
+			seconf.Destroy("config.enc")
+			os.Exit(1)
+		}
+	}
+	if !seconf.Detect("config.enc") && askForConfirmation("No config file found. Would you like to create one?") {
+		err := seconf.Lock("config.enc", "Secured Environment", "Enter the first environmental NAME", "Enter the first environmental VALUE", "Enter the second environmental NAME", "Enter the second environmental VALUE")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if len(os.Args) < 2 {
+			os.Exit(0)
+		}
+	}
+	if !seconf.Detect("config.enc") {
+		fmt.Println("No config.")
+		os.Exit(1)
+	}
+	if len(os.Args) < 2 {
+		fmt.Println("User error: No command supplied.")
+		fmt.Println("Usage: " + os.Args[0] + " ./yourprogram or " + os.Args[0] + " yourprogram ")
+		fmt.Println("Destroy: " + os.Args[0] + " -d")
+		os.Exit(1)
+	}
 
-  configdecoded, err := seconf.Read("config.enc")
-  if err != nil {
-    fmt.Println("error:")
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  configarray := strings.Split(configdecoded, "::::")
-  if len(configarray) < 4 {
-    fmt.Println("Bad config magic.")
-    os.Exit(1)
-  }
-  os.Setenv(configarray[0], configarray[1])
-  os.Setenv(configarray[2], configarray[3])
+	configdecoded, err := seconf.Read("config.enc")
+	if err != nil {
+		fmt.Println("error:")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	configarray := strings.Split(configdecoded, "::::")
+	if len(configarray) < 4 {
+		fmt.Println("Bad config magic.")
+		os.Exit(1)
+	}
+	os.Setenv(configarray[0], configarray[1])
+	os.Setenv(configarray[2], configarray[3])
 
-  cmd := strings.Join(os.Args[1:], " ")
-  out, err := exec.Command("bash", "-c", cmd).Output()
-  if err != nil {
-      fmt.Println(err)
-  }
-  fmt.Printf(string(out))
+	cmd := strings.Join(os.Args[1:], " ")
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf(string(out))
 }
-
-
 
 // constainsString returns true if a slice contains a string.
 func containsString(slice []string, element string) bool {
@@ -90,7 +94,7 @@ func containsString(slice []string, element string) bool {
 // askForConfirmation returns true if the user types one of the "okayResponses"
 // https://gist.github.com/albrow/5882501
 func askForConfirmation(p string) bool {
-  fmt.Println(p)
+	fmt.Println(p)
 	var response string
 	_, err := fmt.Scanln(&response)
 	if err != nil {
